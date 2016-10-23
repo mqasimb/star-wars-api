8f7c87b96c012e61af1fa9f6caeb22a9e959d576
mqasimb <bqasim381@gmail.com>
2016-10-23 10:58:40 -0400

    improved design and add character list

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var query;
 var SWSearchTerm;
 var SWAPIURL = "https://swapi.co/api/people/";
-var BINGURL;
 var WIKIAURLID = "https://starwars.wikia.com/api/v1/Articles/AsSimpleJson";
 var WIKIAURLSearch = "https://starwars.wikia.com/api/v1/Search/List";
-
+//Loading List each time takes some time
+var charList = ["Luke Skywalker", "C-3PO", "R2-D2", "Darth Vader", "Leia Organa", "Owen Lars", "Beru Whitesun lars", 
+"R5-D4", "Biggs Darklighter", "Obi-Wan Kenobi", "Anakin Skywalker", "Wilhuff Tarkin", "Chewbacca", "Han Solo", "Greedo", 
+"Jabba Desilijic Tiure", "Wedge Antilles", "Jek Tono Porkins", "Yoda", "Palpatine", "Boba Fett", "IG-88", "Bossk", 
+"Lando Calrissian", "Lobot", "Ackbar", "Mon Mothma", "Arvel Crynyd", "Wicket Systri Warrick", "Nien Nunb", "Qui-Gon Jinn", 
+"Nute Gunray", "Finis Valorum", "Jar Jar Binks", "Roos Tarpals", "Rugor Nass", "Ric Olié", "Watto", "Sebulba", "Quarsh Panaka", 
+"Shmi Skywalker", "Darth Maul", "Bib Fortuna", "Ayla Secura", "Dud Bolt", "Gasgano", "Ben Quadinaros", "Mace Windu", 
+"Ki-Adi-Mundi", "Kit Fisto", "Eeth Koth", "Adi Gallia", "Saesee Tiin", "Yarael Poof", "Plo Koon", "Mas Amedda", "Gregar Typho", 
+"Cordé", "Cliegg Lars", "Poggle the Lesser", "Luminara Unduli", "Barriss Offee", "Dormé", "Dooku", "Bail Prestor Organa", 
+"Jango Fett", "Zam Wesell", "Dexter Jettster", "Lama Su", "Taun We", "Jocasta Nu", "Ratts Tyerell", "R4-P17", "Wat Tambor", 
+"San Hill", "Shaak Ti", "Grievous", "Tarfful", "Raymus Antilles", "Sly Moore", "Tion Medon", "Finn", "Rey", "Poe Dameron", 
+"BB8", "Captain Phasma", "Padmé Amidala"];
 
 
 function getDataFromSWAPI(callback) {
@@ -14,6 +24,12 @@ function getDataFromSWAPI(callback) {
 	$.getJSON(SWAPIURL, settings, callback);
 }
 
+function getCharacterList(URL, callback) {
+	var settings = {
+		search: ""
+	};
+	$.getJSON(URL, callback);
+}
 function getDataFromBING(callback) {
         var params = {
             q: SWSearchTerm
@@ -57,6 +73,7 @@ function displaySWAPI(data) {
     getArticleIDFromWIKIA(articleIDDisplay);
 	var listofData = "<ul>";
 	for (var key in data.results[0]) {
+		if(key === "name" || key === "height" || key === "mass" || key === "hair_color" || key === "skin_color" || key === "eye_color" || key === "birth_year" || key === "gender")
 		if(typeof(data.results[0][key])==="string") {
 			listofData += '<li>'+key.toTitleCase()+': '+data.results[0][key].toTitleCase()+'</li>';
 	}
@@ -82,24 +99,80 @@ function displayArticle(data) {
     $(".content-title").html(Title);
     $(".article").html(Article);
 }
+function appendCharacters(data) {
+	for(var i=0; i<data.results.length; i++) {
+		charList.push(data.results[i].name);
+	}
+	if(data.next) {
+		getCharacterList(data.next, appendCharacters);
+	}
+}
+function displayCharacterList() {
+	console.log(charList.length);
+	var divisor = charList.length/3;
+	var list1= "<ul class='column-one'>";
+	var list2= "<ul class='column-two'>";
+	var list3= "<ul class='column-three'>";
+	for(var j=0; j<charList.length; j++) {
+		if(j<divisor) {
+			list1 += "<li><a href='#'>"+charList[j]+"</a></li>";
+		}
+		else if(j<divisor*2) {
+			list2 += "<li><a href='#'>"+charList[j]+"</a></li>";	
+		}
+		else {
+			list3 += "<li><a href='#'>"+charList[j]+"</a></li>";
+		}
+	}
+	list1 += "</ul>";
+	list2 += "</ul>";
+	list3 += "</ul>";
+	$(".column-one").html(list1);
+	$(".column-two").html(list2);
+	$(".column-three").html(list3);
+	console.log(charList);
+}
 
 function getImage(data) {
 	$('.character-image').html('<img src="'+data.value[0].contentUrl+'">');
 }
 
 function onSearch() {
+	// getCharacterList(SWAPIURL, appendCharacters);
+	$(".view-list").click(function(event) {
+		event.preventDefault();
+		displayCharacterList();
+		$(".character-list").css("visibility","visible");
+		$(".content-overlay-list").css("visibility","visible");
+		$(".overlay-large-list").css("visibility","visible");
+	});
+	$(".character-list").on("click","a", function(event) {
+		event.preventDefault();
+		query = $(this).text();
+		getDataFromSWAPI(displaySWAPI);
+		$(".character-list").css("visibility","hidden");
+		$(".content-overlay-list").css("visibility","hidden");
+		$(".overlay-large-list").css("visibility","hidden");
+		$(".content-overlay").css("visibility","visible");
+		$(".overlay-large").css("visibility","visible");
+
+	});
 	$(".js-search-form").submit(function(event) {
 		event.preventDefault();
 		query = $("input").val();
 		getDataFromSWAPI(displaySWAPI);
+		$(".character-list").css("visibility","hidden");
 		$(".content-overlay").css("visibility","visible");
 		$(".overlay-large").css("visibility","visible");
 	});
-	$(".overlay-large").click(function(event) {
+	$(".close-box").click(function(event) {
 	    event.stopPropagation();
+	    $(".character-list").css("visibility","hidden");
 		$(".overlay-large").css("visibility","hidden");
 		$(".content-overlay").css("visibility","hidden");
-		$("img ul").remove();
+		$(".overlay-large-list").css("visibility","hidden");
+		$(".content-overlay-list").css("visibility","hidden");
+		$("img, ul, p").remove();
 	});
 }
 String.prototype.toTitleCase = function(){
