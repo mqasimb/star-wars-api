43e4380327650688798060b7e53af8af04cd4acc
Qasim Bhatti <Qasim Bhatti>
2017-04-19 11:56:33 -0400

    spacing changes

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ var app = express();
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/getWikiaID', function(req, res) {  
-  var query = req._parsedUrl.search;
-  client.get("https://starwars.wikia.com/api/v1/Search/List"+query, function (data, response) {
-    res.json(data);
-});
+  	var query = req._parsedUrl.search;
+  	client.get("https://starwars.wikia.com/api/v1/Search/List"+query, function (data, response) {
+    	res.json(data);
+	});
 });
 
 app.get('/getWikiaArticle', function(req, res) {  
-  var query = req._parsedUrl.search;
-  client.get("https://starwars.wikia.com/api/v1/Articles/AsSimpleJson"+query, function (data, response) {
-    res.json(data);
-});
+  	var query = req._parsedUrl.search;
+  	client.get("https://starwars.wikia.com/api/v1/Articles/AsSimpleJson"+query, function (data, response) {
+    	res.json(data);
+	});
 });
 
 app.listen(process.env.PORT || 8080, process.env.IP, function() {
-  console.log('Server running...')
+  	console.log('Server running...')
 });
